1075fb85746b35fcc5673471d16fa0aae4681c23
john-baxter <john.g.baxter@googlemail.com>
2020-04-20 16:07:09 +0100

    first unit test - account has balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,10 @@
 class Bank
 
+  def initialize
+    @account_balance = 0.00
+  end
+
+  attr_reader :account_balance
+
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,4 +2,10 @@ require 'bank.rb'
 
 describe Bank do
 
+  it "has balance" do
+    bank = Bank.new
+    expect(bank.account_balance).to eq (0.00)
+  end
+
+
 end
